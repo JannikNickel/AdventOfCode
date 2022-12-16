0469515cbc27bbfa6b5bb72183451c37c80a1eff
Jannik Nickel <nickel.jannik1997@gmail.com>
2022-12-16 22:31:19 +0100

    Day 16 part 1 solution

--- AdventOfCode2022/Day16/Solution.cs
+++ AdventOfCode2022/Day16/Solution.cs
@@ -12,28 +12,13 @@ namespace AdventOfCode2022.Day16
     {
         public Solution() : base(16, "Proboscidea Volcanium")
         {
-            
+
         }
 
         public override object? SolveFirst()
         {
-            //To skip more permutations
-            //TODO calc distances between all nodes
-            //TODO calc if paths are even possible?
-
             (Valve start, Valve[] valves) = ParseInput(Input.Lines);
-            IEnumerable<Valve> reducedValveSet = valves.Where(n => n == start || n.FlowRate > 0);
-            int[,] distanceMatrix = CalcDistanceMatrix(valves);
-            int max = 0;
-            foreach(var order in Permutations(reducedValveSet, reducedValveSet.Count()))
-            {
-                if(order.First().Name != start.Name)
-                {
-                    break;
-                }
-                max = Math.Max(max, SimulateOrder(order, distanceMatrix));
-            }
-            return max;
+            return FindHighestPressure(start, valves.ToArray());
         }
 
         public override object? SolveSecond()
@@ -41,105 +26,77 @@ namespace AdventOfCode2022.Day16
             return null;
         }
 
-        private int[,] CalcDistanceMatrix(Valve[] valves)
+        private int FindHighestPressure(Valve start, Valve[] valves)
         {
-            int amount = valves.Length;
-            int[,] matrix = new int[amount, amount];
-            for(int i = 0;i < amount;i++)
+            int max = 0;
+            Stack<State> options = new Stack<State>();
+            Dictionary<StateIdentifier, int> stateScoreMemory = new Dictionary<StateIdentifier, int>();
+            options.Push(new State(start, new BitMask(), 0, 0, 30));
+            while(options.Count > 0)
             {
-                for(int k = i + 1;k < amount;k++)
+                State state = options.Pop();
+                Valve v = state.current;
+
+                StateIdentifier sId = new StateIdentifier(state.timeLeft, v.Index);
+                if(stateScoreMemory.TryGetValue(sId, out int released) && released >= state.released)
                 {
-                    int dst = CalcValveDistance(valves[i], valves[k], amount);
-                    matrix[i, k] = dst;
-                    matrix[k, i] = dst;
+                    continue;
                 }
-            }
-            return matrix;
-        }
+                stateScoreMemory[sId] = state.released;
+                max = Math.Max(max, state.released);
 
-        private int CalcValveDistance(Valve from, Valve to, int valveCount)
-        {
-            Queue<Valve> open = new Queue<Valve>();
-            HashSet<Valve> closed = new HashSet<Valve>();
-            open.Enqueue(from);
-            int[] depths = new int[valveCount];
-            
-            while(open.Count > 0)
-            {
-                Valve v = open.Dequeue();
-                closed.Add(v);
-
-                if(v == to)
+                if(state.timeLeft <= 1)
                 {
-                    return depths[v.Index];
+                    continue;
                 }
 
+                if(v.FlowRate > 0 && state.used[v.Index] == false)
+                {
+                    BitMask used = state.used;
+                    used[v.Index] = true;
+                    int p = state.pressure + v.FlowRate;
+                    options.Push(new State(v, used, state.released + p, p, state.timeLeft - 1));
+                }
                 foreach(Valve n in v.Tunnels)
                 {
-                    if(closed.Contains(n) == false)
-                    {
-                        open.Enqueue(n);
-                        depths[n.Index] = depths[v.Index] + 1;
-                    }
+                    options.Push(new State(n, state.used, state.released + state.pressure, state.pressure, state.timeLeft - 1));
                 }
             }
-
-            return -1;
-        }
-
-        private IEnumerator<Valve> Iterator(IEnumerable<Valve> collection)
-        {
-            foreach(Valve valve in collection)
-            {
-                yield return valve;
-            }
-        }
-
-        private int SimulateOrder(IEnumerable<Valve> order, int[,] distanceMatrix)
-        {
-            int sum = 0;
-            int minute = 30;
-            IEnumerator<Valve> iterator = Iterator(order);
-            iterator.MoveNext();
-            Valve current = iterator.Current;
-            do
-            {
-                if(current.FlowRate != 0)
-                {
-                    minute--;
-                    sum += current.FlowRate * minute;
-                }
-                if(!iterator.MoveNext())
-                {
-                    break;
-                }
-                Valve next = iterator.Current;
-                minute -= distanceMatrix[current.Index, next.Index];
-                current = next;
-            }
-            while(minute > 0);
-
-            return sum;
-        }
-
-        private IEnumerable<IEnumerable<T>> Permutations<T>(IEnumerable<T> collection, int length)
-        {
-            if(length == 1)
-            {
-                return collection.Select(n => new T[] { n });
-            }
-            return Permutations(collection, length - 1)
-                .SelectMany(n => collection.Where(k => n.Contains(k) == false), (t1, t2) => t1.Concat(new T[] { t2 }));
+            return max;
         }
 
         private (Valve root, Valve[] valves) ParseInput(string[] lines)
         {
-            Valve[] valves = lines.OrderByDescending(n => n.StartsWith("Valve AA")).Select((line, index) => new Valve(Regex.Match(line, @"[A-Z][A-Z]").Value, int.Parse(Regex.Match(line, @"\d+").Value), index)).ToArray();
+            Valve[] valves = lines.Select((line, index) => new Valve(Regex.Match(line, @"[A-Z][A-Z]").Value, int.Parse(Regex.Match(line, @"\d+").Value), index)).ToArray();
             for(int i = 0;i < lines.Length;i++)
             {
                 valves[i].Tunnels = Regex.Matches(lines[i], @"[A-Z][A-Z]").Skip(1).Select(n => valves.First(k => k.Name == n.Value)).ToArray();
             }
             return (valves.First(n => n.Name == "AA"), valves);
+        }
+
+        private readonly record struct State(Valve current, BitMask used, int released, int pressure, int timeLeft);
+        private readonly record struct StateIdentifier(int valveIndex, int time);
+
+        public struct BitMask
+        {
+            public long data;
+
+            public bool this[int index]
+            {
+                get => (data & (1L << index)) != 0;
+                set
+                {
+                    if(value)
+                    {
+                        data |= (1L << index);
+                    }
+                    else
+                    {
+                        data &= ~(1L << index);
+                    }
+                }
+            }
         }
 
         public class Valve
@@ -154,12 +111,10 @@ namespace AdventOfCode2022.Day16
                 Name = name;
                 FlowRate = flowRate;
                 Index = index;
+                Tunnels = Array.Empty<Valve>();
             }
 
-            public override string ToString()
-            {
-                return $"{Name} ({FlowRate})";
-            }
+            public override string ToString() => $"{Index} > {Name} ({FlowRate})";
         }
     }
 }
