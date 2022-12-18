c1583abab68b2278b30c111cbae27750451541d4
Jannik Nickel <nickel.jannik1997@gmail.com>
2022-12-18 15:08:51 +0100

    Day 16 part 2 solution fix
    Works for the regular input now. It's quite a bit slower and part 2 could probably be optimized based on the previous version of this solution by applying the same fix. For now, part 2 simulates all possible ways to split the valves into two sets which results in ~16k simulations which is quite manageable, especially with multithreading

--- AdventOfCode2022/Day16/Solution.cs
+++ AdventOfCode2022/Day16/Solution.cs
@@ -17,28 +17,39 @@ namespace AdventOfCode2022.Day16
 
         public override object? SolveFirst()
         {
-            (Valve start, Valve[] valves) = ParseInput(Input.Lines);
-            return FindHighestPressure(start, valves.ToArray(), 30);
+            (Valve start, _) = ParseInput(Input.Lines);
+            return FindHighestPressure(start, new BitMask(), 30);
         }
 
         public override object? SolveSecond()
         {
             (Valve start, Valve[] valves) = ParseInput(Input.Lines);
-            return FindHighestPressureTogether(start, valves.ToArray(), 26);//2316
+            IEnumerable<IEnumerable<int>>[] splits = Partitions(valves.Where(n => n.FlowRate > 0).Select(n => n.Index).ToArray(), 2).ToArray();
+            int max = 0;
+            object l = new object();
+            Parallel.For(0, splits.Length, (int i) =>
+            {
+                int result = FindHighestPressure(start, new BitMask(splits[i].First()), 26) + FindHighestPressure(start, new BitMask(splits[i].Skip(1).First()), 26);
+                lock(l)
+                {
+                    max = Math.Max(max, result);
+                }
+            });
+            return max;
         }
 
-        private int FindHighestPressure(Valve start, Valve[] valves, int time)
+        private int FindHighestPressure(Valve start, BitMask initalUsed, int time)
         {
             int max = 0;
             Stack<State> options = new Stack<State>();
             Dictionary<StateIdentifier, int> stateScoreMemory = new Dictionary<StateIdentifier, int>();
-            options.Push(new State(start, new BitMask(), 0, 0, time));
+            options.Push(new State(start, initalUsed, 0, 0, time));
             while(options.Count > 0)
             {
                 State state = options.Pop();
                 Valve v = state.current;
 
-                StateIdentifier sId = new StateIdentifier(v.Index, state.timeLeft);
+                StateIdentifier sId = new StateIdentifier(v.Index, state.used, state.timeLeft);
                 if(stateScoreMemory.TryGetValue(sId, out int released) && released >= state.released)
                 {
                     continue;
@@ -66,67 +77,6 @@ namespace AdventOfCode2022.Day16
             return max;
         }
 
-        private int FindHighestPressureTogether(Valve start, Valve[] valves, int time)
-        {
-            int max = 0;
-            Stack<StateV2> options = new Stack<StateV2>();
-            Dictionary<StateIdentifierV2, int> stateScoreMemory = new Dictionary<StateIdentifierV2, int>();
-            options.Push(new StateV2(start, start, new BitMask(), 0, 0, time));
-            while(options.Count > 0)
-            {
-                StateV2 state = options.Pop();
-                Valve v = state.current;
-                Valve e = state.elephant;
-
-                StateIdentifierV2 sId = new StateIdentifierV2(v.Index, e.Index, state.timeLeft);
-                if(stateScoreMemory.TryGetValue(sId, out int released) && released >= state.released)
-                {
-                    continue;
-                }
-                stateScoreMemory[sId] = state.released;
-                max = Math.Max(max, state.released);
-
-                if(state.timeLeft <= 1)
-                {
-                    continue;
-                }
-
-                if(v.FlowRate > 0 && state.used[v.Index] == false)
-                {
-                    BitMask used = state.used;
-                    used[v.Index] = true;
-
-                    AddElephantValveOpenIfPossible(options, v, e, state, used, v.FlowRate + e.FlowRate);
-                    AddElephantMoves(options, v, e, state, used, v.FlowRate);
-                }
-                foreach(Valve vTunnel in v.Tunnels)
-                {
-                    AddElephantValveOpenIfPossible(options, vTunnel, e, state, state.used, e.FlowRate);
-                    AddElephantMoves(options, vTunnel, e, state, state.used, 0);
-                }
-            }
-            return max;
-        }
-
-        private void AddElephantValveOpenIfPossible(Stack<StateV2> options, Valve v, Valve e, StateV2 state, BitMask used, int pressureIncrease)
-        {
-            if(e.FlowRate > 0 && used[e.Index] == false)
-            {
-                used[e.Index] = true;
-                int p = state.pressure + pressureIncrease;
-                options.Push(new StateV2(v, e, used, state.released + p, p, state.timeLeft - 1));
-            }
-        }
-
-        private void AddElephantMoves(Stack<StateV2> options, Valve v, Valve e, StateV2 state, BitMask used, int pressureIncrease)
-        {
-            foreach(Valve eTunnel in e.Tunnels)
-            {
-                int p = state.pressure + pressureIncrease;
-                options.Push(new StateV2(v, eTunnel, used, state.released + p, p, state.timeLeft - 1));
-            }
-        }
-
         private (Valve root, Valve[] valves) ParseInput(string[] lines)
         {
             Valve[] valves = lines.Select((line, index) => new Valve(Regex.Match(line, @"[A-Z][A-Z]").Value, int.Parse(Regex.Match(line, @"\d+").Value), index)).ToArray();
@@ -137,14 +87,50 @@ namespace AdventOfCode2022.Day16
             return (valves.First(n => n.Name == "AA"), valves);
         }
 
+        private IEnumerable<IEnumerable<IEnumerable<T>>> Partitions<T>(T[] source, int splits)
+        {
+            IEnumerable<IEnumerable<IEnumerable<T>>> Generate(List<List<T>> groups, int length, int index)
+            {
+                if(index >= length)
+                {
+                    yield return new List<List<T>>(groups.Select(g => g.ToList()));
+                }
+                else
+                {
+                    if(length - index > splits - groups.Count)
+                    {
+                        foreach(List<T> group in new List<List<T>>(groups))
+                        {
+                            group.Add(source[index]);
+                            foreach(IEnumerable<IEnumerable<T>> part in Generate(groups, length, index + 1))
+                            {
+                                yield return part;
+                            }
+                            group.RemoveAt(group.Count - 1);
+                        }
+                    }
+
+                    if(groups.Count < splits)
+                    {
+                        groups.Add(new List<T>() { source[index] });
+                        foreach(IEnumerable<IEnumerable<T>> part in Generate(groups, length, index + 1))
+                        {
+                            yield return part;
+                        }
+                        groups.RemoveAt(groups.Count - 1);
+                    }
+                }
+            }
+
+            return Generate(new List<List<T>>(), source.Length, 0);
+        }
+
         private readonly record struct State(Valve current, BitMask used, int released, int pressure, int timeLeft);
-        private readonly record struct StateIdentifier(int valveIndex, int time);
-        private readonly record struct StateV2(Valve current, Valve elephant, BitMask used, int released, int pressure, int timeLeft);
-        private readonly record struct StateIdentifierV2(int valveIndex, int elefantIndex, int time);
+        private readonly record struct StateIdentifier(int valveIndex, long used, int time);
 
         public struct BitMask
         {
-            public long data;
+            private long data;
 
             public bool this[int index]
             {
@@ -160,6 +146,20 @@ namespace AdventOfCode2022.Day16
                         data &= ~(1L << index);
                     }
                 }
+            }
+
+            public BitMask(IEnumerable<int> indices)
+            {
+                data = 0;
+                foreach(int i in indices)
+                {
+                    this[i] = true;
+                }
+            }
+
+            public static implicit operator long(BitMask mask)
+            {
+                return mask.data;
             }
         }
 
