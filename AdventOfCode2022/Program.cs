d8692a002e11500e4c5cd9b8dbc7640d99c87027
Jannik Nickel <nickel.jannik1997@gmail.com>
2022-12-26 15:45:36 +0100

    Readme + improved program params
    Added readme
    Added --all parameter to run all solutions

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
@@ -13,23 +15,29 @@ namespace AdventOfCode2022
         {
             Console.ForegroundColor = defaultColor;
 
-            SolutionBase? solution = null;
+            IEnumerable<SolutionBase>? solutions = null;
             if(args.Length > 0)
             {
-                if(int.TryParse(args[0], out int day))
+                if(args[0] == "--day" && int.TryParse(args[1], out int day))
                 {
                     if(day >= 1 && day <= 25)
                     {
-                        solution = SolutionBase.Solutions().FirstOrDefault(n => n.day == day);
+                        SolutionBase? solution = SolutionBase.Solutions().FirstOrDefault(n => n.day == day);
+                        solutions = solution != null ? new SolutionBase[] { solution } : null;
                     }
+                }
+                else if(args[0] == "--all")
+                {
+                    solutions = SolutionBase.Solutions();
                 }
             }
 
-            if(solution == null)
+            if(solutions == null)
             {
                 Console.WriteLine("Fallback to last solution!");
-                solution = SolutionBase.Solutions().LastOrDefault();
-                if(solution == null)
+                SolutionBase? solution = SolutionBase.Solutions().LastOrDefault();
+                solutions = solution != null ? new SolutionBase[] { solution } : null;
+                if(solutions == null)
                 {
                     Console.WriteLine("No solutions found!");
                     Console.ReadKey();
@@ -38,13 +46,14 @@ namespace AdventOfCode2022
                 Console.WriteLine();
             }
 
-            Console.WriteLine($"Preparing Solution {solution.day:D2} - {solution.title}");
-            solution.Prepare();
+            foreach(SolutionBase solution in solutions)
+            {
+                Console.WriteLine($"Preparing Solution {solution.day:D2} - {solution.title}");
+                solution.Prepare();
 
-            Solve(solution, 1);
-            Solve(solution, 2);
-
-            Console.ReadLine();
+                Solve(solution, 1);
+                Solve(solution, 2);
+            }
         }
 
         private static void Solve(SolutionBase solution, int part)
@@ -52,6 +61,7 @@ namespace AdventOfCode2022
             Console.WriteLine($"Solving Part {part}...");
             Stopwatch sw = Stopwatch.StartNew();
             object? result = part == 1 ? solution.SolveFirst() : solution.SolveSecond();
+            sw.Stop();
             Console.WriteLine($"Solved Part {part} ({Math.Round(sw.Elapsed.TotalMilliseconds, 2)}ms)");
             Console.WriteLine("Result:");
             Console.ForegroundColor = resultColor;
