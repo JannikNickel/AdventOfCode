de1f1bc7f77135e79d2e056ca919e52c83fa4587
Jannik Nickel <nickel.jannik1997@gmail.com>
2023-02-12 21:05:00 +0100

    Day01 solution

--- 2021/src/solutions/day01.rs
+++ 2021/src/solutions/day01.rs
@@ -8,10 +8,26 @@ impl Solution for Day01 {
     fn name(&self) -> &'static str { "Dive!" }
 
     fn run1(&self, input: &Input) -> Option<Box<dyn std::fmt::Display>> {
-        None
+        let mut amount = 0;
+        for i in 1..input.lines.len() {
+            let prev = input.lines[i - 1].parse::<i32>().unwrap();
+            if input.lines[i].parse::<i32>().unwrap() > prev {
+                amount += 1;
+            }
+        }
+
+        Some(Box::new(amount))
     }
 
     fn run2(&self, input: &Input) -> Option<Box<dyn std::fmt::Display>> {
-        None
+        let mut amount = 0;
+        let mut prev = i32::MAX;
+        for i in input.lines.windows(3).map(|n| n[0].parse::<i32>().unwrap() + n[1].parse::<i32>().unwrap() + n[2].parse::<i32>().unwrap()) {
+            if i > prev {
+                amount += 1;
+            }
+            prev = i;
+        }
+        Some(Box::new(amount))
     }
 }
