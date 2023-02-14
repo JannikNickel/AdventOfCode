01a019d777fa049aba8ee7032935e51d22bd4315
Jannik Nickel <nickel.jannik1997@gmail.com>
2023-02-14 02:38:52 +0100

    Day06 solution

--- 2021/src/solutions.rs
+++ 2021/src/solutions.rs
@@ -3,12 +3,14 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 pub use day01::Day01;
 pub use day02::Day02;
 pub use day03::Day03;
 pub use day04::Day04;
 pub use day05::Day05;
+pub use day06::Day06;
 use crate::input::Input;
 
 pub trait Solution {
@@ -24,7 +26,8 @@ pub fn all_solutions() -> Vec<Box<dyn Solution>> {
         Box::new(Day02),
         Box::new(Day03),
         Box::new(Day04),
-        Box::new(Day05)
+        Box::new(Day05),
+        Box::new(Day06)
     ];
     v
 }
