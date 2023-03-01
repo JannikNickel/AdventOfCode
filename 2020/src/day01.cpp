d6703ff11e1dbeeb265942f8919bf975807d9633
Jannik Nickel <nickel.jannik1997@gmail.com>
2023-03-01 22:29:47 +0100

    Year 2020, Day 01 solution

--- 2020/src/day01.cpp
+++ 2020/src/day01.cpp
@@ -1,11 +1,46 @@
 #include "day01.h"
+#include <ranges>
 
 ResultBase* Day01::SolveFirst() const
 {
-	return new Result(":)");
+	std::vector<int> numbers = ParseInput();
+	for(int i = 0; i < numbers.size(); i++)
+	{
+		for(int k = i + 1; k < numbers.size(); k++)
+		{
+			if(numbers[i] + numbers[k] == 2020)
+			{
+				return new Result(numbers[i] * numbers[k]);
+			}
+		}
+	}
+	return nullptr;
 }
 
 ResultBase* Day01::SolveSecond() const
 {
-	return new ResultBase();
+	std::vector<int> numbers = ParseInput();
+	for(int i = 0; i < numbers.size(); i++)
+	{
+		for(int k = i + 1; k < numbers.size(); k++)
+		{
+			for(int l = k + 1; l < numbers.size(); l++)
+			{
+				if(numbers[i] + numbers[k] + numbers[l] == 2020)
+				{
+					return new Result(numbers[i] * numbers[k] * numbers[l]);
+				}
+			}
+		}
+	}
+	return nullptr;
+}
+
+std::vector<int> Day01::ParseInput() const
+{
+	auto res = input.lines | std::views::transform([](const std::string& str)
+	{
+		return std::stoi(str);
+	});
+	return std::vector<int>(res.begin(), res.end());
 }
--- 2020/src/day01.h
+++ 2020/src/day01.h
@@ -1,14 +1,15 @@
 #pragma once
 #include "solutionbase.h"
+#include <vector>
 
 struct Day01 : SolutionBase
 {
-	Day01(const Input& input) : SolutionBase(input, 1, "") { }
+	Day01(const Input& input) : SolutionBase(input, 1, "Report Repair") { }
 
 protected:
 	ResultBase* SolveFirst() const override;
 	ResultBase* SolveSecond() const override;
 
 private:
-
+	std::vector<int> ParseInput() const;
 };
