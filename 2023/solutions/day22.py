8c6876e116ce6a881249ab94b8b76abfc4407e66
Jannik Nickel <nickel.jannik1997@gmail.com>
2023-12-22 20:53:21 +0100

    Improved day 22 performance
    Improved performance from 90 seconds / part to ~500ms / part by using a dictionary of settled grid positions while simulating brick falls

--- 2023/solutions/day22.py
+++ 2023/solutions/day22.py
@@ -1,6 +1,7 @@
 from input import Input
 from dataclasses import dataclass
 from collections import defaultdict
+from collections.abc import Generator
 
 @dataclass(eq = True, unsafe_hash = True)
 class Vec3():
@@ -15,26 +16,33 @@ class Bounds():
 
     def contains(self, p: Vec3) -> bool:
         return self.min.x <= p.x and p.x <= self.max.x and self.min.y <= p.y and p.y <= self.max.y and self.min.z <= p.z and p.z <= self.max.z
+    
+    def points(self) -> Generator[Vec3, None, None]:
+        for x in range(self.min.x, self.max.x + 1):
+            for y in range(self.min.y, self.max.y + 1):
+                for z in range(self.min.z, self.max.z + 1):
+                    yield Vec3(x, y, z)
 
 def parse_input(input: Input) -> list[Bounds]:
     return [Bounds(Vec3(*[int(n) for n in l.split(",")]), Vec3(*[int(n) for n in r.split(",")])) for l, r in (line.split("~") for line in input.lines)]
 
-def search_area(bricks: list[Bounds], b: Bounds, z_off: int) -> set[Bounds]:
+def search_area(settled: dict[Vec3, Bounds], b: Bounds, z_off: int) -> set[Bounds]:
     res = set()
     for x in range(b.min.x, b.max.x + 1):
         for y in range(b.min.y, b.max.y + 1):
             p = Vec3(x, y, b.min.z + z_off)
-            below = [bl for bl in bricks if bl is not b and bl.contains(p)]
-            res |= set(below)
+            if below := settled.get(p, None):
+                res.add(below)
     return res
 
 def simulate_fall(bricks: list[Bounds]) -> tuple[dict[Bounds, set[Bounds]], dict[Bounds, set[Bounds]]]:
     bricks = sorted(bricks, key = lambda b: b.min.z)
     upwards = defaultdict(set)
     downwards = defaultdict(set)
+    settled = dict()
     for b in bricks:
         while b.min.z > 1:
-            below = search_area(bricks, b, -1)
+            below = search_area(settled, b, -1)
             if len(below) > 0:
                 for be in below:
                     upwards[be].add(b)
@@ -42,6 +50,8 @@ def simulate_fall(bricks: list[Bounds]) -> tuple[dict[Bounds, set[Bounds]], dict
                 break
             b.min.z -= 1
             b.max.z -= 1
+        for pos in b.points():
+            settled[pos] = b
     return upwards, downwards
 
 def find_disconnected(up, down, closed, brick):
