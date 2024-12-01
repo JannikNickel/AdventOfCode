c11fead973cfd83a56fd8c65c817dc36a1d651e1
Jannik Nickel <nickel.jannik1997@gmail.com>
2024-12-01 13:48:17 +0100

    Advent Of Code 2024 in F#

---
+++ 2024/src/common.fs
@@ -0,0 +1,27 @@
+﻿module Common
+
+module List = 
+    let split (pred: 'a -> bool) (list: 'a list) = 
+        list
+        |> List.fold (fun (acc, curr) n -> if pred n then (acc @ [curr], []) else (acc, n :: curr)) ([], [])
+        |> fun pair -> fst pair @ [snd pair]
+        |> List.map (fun n -> List.rev n)
+
+    let windowedi (windowSize: int) (list: 'T list) = 
+        list
+        |> List.mapi (fun i _ -> i, list |> List.skip i |> List.truncate windowSize)
+        |> List.filter (fun (_, wnd) -> wnd.Length = windowSize)
+
+module Parsing = 
+    open System.Text.RegularExpressions
+
+    let ints (str: string) = 
+        let matches = Regex.Matches (str, @"-?\d+")
+        [ for m in matches -> int m.Value ]
+
+    let map2d (mapping: char -> 'T) (lines: string list) : ((int * int) * 'T) seq = 
+        seq {
+            for x in 0 .. lines[0].Length - 1 do
+                for y in 0 .. lines.Length - 1 do
+                    yield (x, y), mapping lines[y].[x]
+        }
---
+++ 2024/src/day01.fs
@@ -0,0 +1,15 @@
+﻿module Day01
+open Input
+open Common
+
+[<Literal>]
+let name = ""
+
+let parseInput (input: Input) = 
+    null
+
+let solveFirst (input: Input) = 
+    null :> obj
+
+let solveSecond (input: Input) = 
+    null :> obj
