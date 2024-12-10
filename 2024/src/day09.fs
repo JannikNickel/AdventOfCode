9b1195e0fa52f43af0206e49bef4130b895651be
Jannik Nickel <nickel.jannik1997@gmail.com>
2024-12-10 17:37:43 +0100

    Day 09 solution cleanup

--- 2024/src/day09.fs
+++ 2024/src/day09.fs
@@ -6,12 +6,12 @@ open Common
 let name = "Disk Fragmenter"
 
 type Block = 
-| File of int
-| Empty
+    | File of int
+    | Empty
 
 type Sector = 
-| FileSector of int * int
-| EmptySector of int
+    | FileSector of int * int
+    | EmptySector of int
 
 let inline charToInt c = int c - int '0'
 
@@ -25,50 +25,36 @@ let expandDiskMap (map: char seq) =
     |> List.concat
     |> Array.ofList
 
-let parseSectors (map: char seq) = 
+let readSectors (map: char seq) = 
     let files, empty = map |> List.ofSeq |> List.map charToInt |> List.alternate
     let empty = if files.Length <> empty.Length then (empty @ [0]) else empty
     (files, empty)
-    ||> List.mapi2 (fun i f e -> 
-        [ FileSector(f, i); EmptySector(e) ]
-    )
+    ||> List.mapi2 (fun i f e -> [ FileSector(f, i); EmptySector(e) ])
     |> List.concat
     |> Array.ofList
 
 let findLastIdx predicate (lastIdx: int) (disk: Block array) = 
-    let mutable res = -1
-    for i in lastIdx .. -1 .. 0 do
-        if res = -1 && predicate disk[i] then
-            res <- i
-    res
+    let rec search i = 
+        if i < 0 then -1
+        elif predicate disk[i] then i
+        else search (i - 1)
+
+    search lastIdx
 
 let compress (disk: Block array) = 
-    let fCount = (disk |> Array.filter (fun n -> n <> Empty) |> Array.length)
-    let mutable last = disk.Length - 1
+    let fCount = (disk |> Array.filter (fun n -> n.IsFile) |> Array.length)
     disk
-    |> Array.mapi (fun i n -> 
-        if i >= fCount then
-            Empty
-        else
-            match n with
-            | File _ -> n
-            | Empty -> 
-                let lastIdx = disk |> findLastIdx (fun n -> n <> Empty) last
-                last <- lastIdx - 1
-                disk[lastIdx]
-    )
-
-let findSector (fileSecs: Sector array) (maxSize: int) = 
-    let idx = 
-        fileSecs
-        |> Array.tryFindIndex (fun n -> 
-            match n with
-            | FileSector(len, _) -> len <= maxSize
-            | _ -> false
-        )
-    match idx with
-    | Some i -> Some(i, fileSecs[i])
-    | _ -> None
+    |> Array.fold (fun (i, last, acc) n -> 
+        match i, n with
+        | i, _ when i >= fCount -> (i + 1, last, Empty :: acc)
+        | _, File _ -> (i + 1, last, n :: acc)
+        | _, Empty -> 
+            let lastIdx = disk |> findLastIdx (fun n -> n.IsFile) last
+            (i + 1, lastIdx - 1, disk[lastIdx] :: acc)
+    ) (0, disk.Length - 1, [])
+    |> (fun (_, _, n) -> n)
+    |> List.rev
+    |> Array.ofList
 
 let secSize (sec: Sector) = 
     match sec with
@@ -81,9 +67,9 @@ let findSpace (disk: Sector array) (minSpace: int) =
     | Some i -> Some(i, disk[i])
     | _ -> None
     
-let solve (file: Sector) (disk: Sector array): Sector array = 
+let moveFile (file: Sector) (disk: Sector array): Sector array = 
     match file with
-    | EmptySector(_) -> failwith ""
+    | EmptySector _ -> failwith ""
     | FileSector(len, _id) -> 
         let sec = findSpace disk len
         match sec with
@@ -95,85 +81,54 @@ let solve (file: Sector) (disk: Sector array): Sector array =
             )
 
             if secIdx <= fIdx then
-                let lIdx = fIdx - 1
-                let rIdx = fIdx + 1
-                let validL = lIdx >= 0 && disk[lIdx].IsEmptySector
-                let validR = rIdx < disk.Length && disk[rIdx].IsEmptySector
-                let space =
-                    (secSize disk[fIdx])
-                    //+ if validL then (disk[lIdx] |> secSize) else 0
-                    //+ if validR then (disk[rIdx] |> secSize) else 0
-                let disk =
-                    disk
-                    |> Array.removeAt fIdx
-                    |> Array.insertAt fIdx (EmptySector(space))
-                    //|> if validR then (Array.removeAt rIdx) else id
-                    //|> if validL then (Array.removeAt lIdx) else id
-
+                let space = (secSize disk[fIdx])
                 let secSize = secSize sec
                 let diff = secSize - len
-                let disk = 
-                    disk
-                    |> Array.removeAt secIdx
-                    |> Array.insertAt secIdx file
-                    |> if diff > 0 then Array.insertAt (secIdx + 1) (EmptySector(diff)) else id
+
                 disk
+                |> Array.removeAt fIdx
+                |> Array.insertAt fIdx (EmptySector(space))
+                |> Array.removeAt secIdx
+                |> Array.insertAt secIdx file
+                |> if diff > 0 then Array.insertAt (secIdx + 1) (EmptySector(diff)) else id
             else
                 disk
         | _ -> disk
 
-let compressSectors (disk: Sector array): Sector list = 
+let compressSectors (disk: Sector array) = 
     let disk = disk |> Array.filter (fun n -> n |> secSize > 0)
-    let files =
-        disk
-        |> Array.filter (fun n -> match n with FileSector(_, _) -> true | _ -> false)
-        |> Array.rev
 
-    files
-    |> Array.fold (fun acc n -> 
-        solve n acc
-    ) disk
-    |> List.ofArray
-
-let checksum disk = 
     disk
-    |> Array.mapi (fun i n -> 
-        i * (match n with | File id -> id | _ -> 0)
-    )
+    |> Array.filter (fun n -> match n with FileSector(_, _) -> true | _ -> false)
+    |> Array.rev
+    |> Array.fold (fun acc n -> moveFile n acc) disk
 
-let expandSectors disk = 
+let expandSectors (disk: Sector array) = 
     disk
     |> Seq.filter (fun n -> n |> secSize > 0)
     |> Seq.collect (fun n -> 
         match n with
-        | FileSector(len, id) -> [for _ in [ 0 .. (len - 1)] -> File(id) ]
-        | EmptySector(len) -> [for _ in [ 0 .. (len - 1)] -> Empty ]
+        | FileSector(len, id) -> [ for _ in [ 0 .. (len - 1)] -> File(id) ]
+        | EmptySector(len) -> [ for _ in [ 0 .. (len - 1)] -> Empty ]
     )
     |> Array.ofSeq
 
-let dbgPrint (list: Sector list) = 
-    list
-    |> List.iter (fun n -> 
-        let s =
-            match n with
-            | FileSector(len, id) -> [ for _ in [0..(len - 1)] -> sprintf "%d" id ]
-            | EmptySector(len) -> [ for _ in [0..(len - 1)] -> sprintf "." ]
-        printf "%A" s
+let checksum disk = 
+    disk |> Array.mapi (fun i n -> 
+        (uint64 i) * (match n with | File id -> uint64 id | _ -> 0UL)
     )
 
 let solveFirst (input: Input) = 
-    let disk = input.raw |> expandDiskMap |> compress |> checksum
-    let mutable sum = 0UL
-    for v in disk do
-        sum <- sum + (v |> uint64)
-    sum
+    input.raw
+    |> expandDiskMap
+    |> compress
+    |> checksum
+    |> Array.sum
 
 let solveSecond (input: Input) = 
-    let disk = input.raw |> parseSectors
-    let disk = disk |> compressSectors |> List.filter (fun n -> n |> secSize > 0)
-    let disk = disk |> expandSectors
-    let disk = disk |> checksum
-    let mutable sum = 0UL
-    for v in disk do
-        sum <- sum + (v |> uint64)
-    sum
+    input.raw
+    |> readSectors
+    |> compressSectors
+    |> expandSectors
+    |> checksum
+    |> Array.sum
