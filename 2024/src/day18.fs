179335a538ae5709e145924008e97c8711a9e411
Jannik Nickel <nickel.jannik1997@gmail.com>
2024-12-18 18:58:38 +0100

    Day 18 simplified
    No need to create a full Map to represent the grid

--- 2024/src/day18.fs
+++ 2024/src/day18.fs
@@ -12,16 +12,7 @@ let parseInput (input: Input) =
     input.lines
     |> List.map (Parsing.ints >> (function [a; b] -> Vec2(a, b) | _ -> Vec2(0, 0)))
 
-let constructGrid (bytes: Vec2 list) = 
-    let set = bytes |> Set.ofList
-    seq {
-        for x in 0 .. size.x do
-            for y in 0 .. size.y -> Vec2(x, y)
-    }
-    |> Seq.map (fun n -> (n, set |> Set.contains n |> not))
-    |> Map.ofSeq
-
-let rec bfs (map: Map<Vec2, bool>) (dest: Vec2) (closed: Set<Vec2>) (queue: (Vec2 * int) list) = 
+let rec bfs (obstacles: Set<Vec2>) (dest: Vec2) (closed: Set<Vec2>) (queue: (Vec2 * int) list) = 
     let bounds = size + Vec2.one
     match queue with
     | (curr, depth) :: tail -> 
@@ -30,20 +21,20 @@ let rec bfs (map: Map<Vec2, bool>) (dest: Vec2) (closed: Set<Vec2>) (queue: (Vec
         else
             let queue = tail
             if closed |> Set.contains curr then
-                bfs map dest closed queue
+                bfs obstacles dest closed queue
             else
                 let closed = closed |> Set.add curr
                 let next = 
                     dirs
                     |> List.map (fun n -> (curr + n, depth + 1))
                     |> List.filter (fst >> (Vec2.inBounds bounds))
-                    |> List.filter (fst >> (fun n -> map |> Map.find n))
+                    |> List.filter (fst >> (fun n -> obstacles |> Set.contains n |> not))
                 let queue = queue @ next
-                bfs map dest closed queue
+                bfs obstacles dest closed queue
     | _ -> -1
 
 let findDepth (bytes: Vec2 list) (dropAmount: int) = 
-    let map = constructGrid bytes[ .. (dropAmount - 1) ]
+    let map = bytes[ .. (dropAmount - 1) ] |> Set.ofList
     bfs map size Set.empty [(Vec2(0, 0), 0)]
 
 let rec findMinBlocked (bytes: Vec2 list) (dropRange: int * int) = 
